0aa0a7fe03a113b7601121009b81d60897379926
glennr <glenn@siyelo.com>
2013-03-11 12:06:04 +0200

    bump 0.3.3, update authors

--- spec/opensrs/version_spec.rb
+++ spec/opensrs/version_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe OpenSRS::Version do
   describe "VERSION" do
     it "should return version string" do
-      OpenSRS::Version::VERSION.should eql("0.3.2")
+      OpenSRS::Version::VERSION.should eql("0.3.3")
     end
   end
 end
