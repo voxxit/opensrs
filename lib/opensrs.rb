99f0b10e3bceed285b9666b2becbef8849b62a67
vveliev <vveliev@tucows.com>
2016-07-26 14:08:41 -0400

    fixing white space related back within xml processing

--- lib/opensrs.rb
+++ lib/opensrs.rb
@@ -1,8 +1,8 @@
-require "opensrs/xml_processor"
-require "opensrs/xml_processor/libxml"
-require "opensrs/server"
-require "opensrs/response"
-require "opensrs/version"
+require File.dirname(__FILE__) + '/opensrs/xml_processor'
+require File.dirname(__FILE__) + '/opensrs/xml_processor/libxml'
+require File.dirname(__FILE__) + '/opensrs/server'
+require File.dirname(__FILE__) + '/opensrs/response'
+require File.dirname(__FILE__) + '/opensrs/version'
 
 module OpenSRS
 end
