fb1e90298b19f830a096568e912b5551f9bdb722
vveliev <vveliev@ryerson.ca>
2016-10-05 20:29:17 -0400

    Update opensrs.rb

--- lib/opensrs.rb
+++ lib/opensrs.rb
@@ -1,8 +1,8 @@
-require "/opensrs/xml_processor"
-require "/opensrs/xml_processor/libxml"
-require "/opensrs/server"
-require "/opensrs/response"
-require "/opensrs/version"
+require "opensrs/xml_processor"
+require "opensrs/xml_processor/libxml"
+require "opensrs/server"
+require "opensrs/response"
+require "opensrs/version"
 
 module OpenSRS
 end
